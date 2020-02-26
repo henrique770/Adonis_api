809340fd5d634c327e8bcd29d366d6cd8c1b7b94
henrique770 <henrique.1360@gmail.com>
2020-02-26 17:09:12 -0300

    feat: view files

--- start/routes.js
+++ start/routes.js
@@ -9,4 +9,6 @@ Route.post("sessions", "SessionController.store");
 Route.post("passwords", "ForgotPasswordController.store");
 Route.put("passwords", "ForgotPasswordController.update");
 
+//Rota para acessar o arquivo
+Route.get("/files/:id", "FileController.show");
 Route.post("/files", "FileController.store");
