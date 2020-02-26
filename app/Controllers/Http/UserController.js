51646c8d9e6642b9d60beb5ab3148955cdf19433
henrique770 <henrique.1360@gmail.com>
2020-02-26 13:50:30 -0300

    fix: comment

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,7 +5,7 @@ const User = use("App/Models/User");
 class UserController {
   async store({ request }) {
     //pegar os dados que o usuário está enviando
-    //reques.only pega somente os campos especificados
+    //request.only pega somente os campos especificados
     const data = request.only(["username", "email", "password"]);
     //criar novo usuário
     const user = await User.create(data);
